35ba8a17cd6c9da5220d595580b7e0dc5a799c14
emirhanakturk <emirhanakturk@gmail.com>
2020-12-23 21:45:13 +0300

    InitImage ve display fonksiyonları eklendi.

--- paint3D.py
+++ paint3D.py
@@ -3,9 +3,30 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+from PIL import Image
+
 import sys
+
 windowWidth=1920
 windowHeight=1080
+
+def InitImage():
+    pencilImg = Image.open("./img/kalem.png")
+    xSize = pencilImg.size[0]
+    ySize = pencilImg.size[1]
+    rawReference = pencilImg.tobytes("raw", "RGB")
+
+    glClearColor(0, 0, 0, 0)
+
+    # Create Texture
+    id = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, id)  # bind Texture, 2d texture (x and y size)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, xSize, ySize, 0, GL_RGB, GL_UNSIGNED_BYTE, rawReference)
+    glEnable(GL_TEXTURE_2D)
 
 def InitGL():
     glClearColor(0.0, 0.0, 0.0, 0.0) #darkmode
@@ -15,6 +36,27 @@ def InitGL():
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
+def display():
+    """Glut display function."""
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glColor3f(1, 1, 1)
+    glBegin(GL_QUADS)
+
+    glTexCoord2f(0.2, 0.2)
+    glVertex3f(-0.5, 0.5, 0)
+
+    glTexCoord2f(0.2, 0.69)
+    glVertex3f(-0.5, -0.5, 0)
+
+    glTexCoord2f(0.69, 0.69)
+    glVertex3f(0.5, -0.5, 0)
+
+    glTexCoord2f(0.69, 0)
+    glVertex3f(0.5, 0.5, 0)
+    glEnd()
+
+    glutSwapBuffers()
+
 def paintBackground(r,g,b):
     glColor3f(r,g,b)
     glBegin(GL_QUADS)
@@ -23,6 +65,7 @@ def paintBackground(r,g,b):
     glVertex2f(1, -1)
     glVertex2f(1, 1)
     glEnd()
+
 def controlPanel():
     glViewport(0, 735, 1540, 100)
     paintBackground(1,0,0)
@@ -35,11 +78,7 @@ def controlPanel():
     glViewport(300, 735, 100, 100)
     paintBackground(0, 1, 0)
 
-
-
-
 #def draw():
-
 
 def paint():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -61,6 +100,8 @@ def main():
     glutIdleFunc(paint)
     #glutSpecialFunc(keyPressed)
     InitGL()
+    InitImage()
+    glutDisplayFunc(display)
     glutMainLoop()
 
 
